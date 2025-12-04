ac2904474618c264283cbf0df7455f634d8789fc
Joy <flyimer@mail.ru>
2025-12-04 21:12:39 +0500

    Initial commit

---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+from calculator import add, sub, mul, div
+import pytest
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_sub():
+    assert sub(5, 3) == 2
+
+
+def test_mul():
+    assert mul(4, 3) == 12
+
+
+def test_div():
+    assert div(10, 2) == 5
+
+
+def test_div_by_zero():
+    with pytest.raises(ValueError):
+        div(1, 0)
